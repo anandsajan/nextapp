e0d39e1012d072c3963baa9f05b0876b5f181973
Anand Sajan <40339820+sajananand@users.noreply.github.com>
2024-12-02 20:59:55 +0530

    revamp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
 import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
